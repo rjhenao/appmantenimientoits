05820a187ea8d01ebcc5ea682c32d95e12fc52db
rjhenao <rjhenao@sacyr.com>
2025-11-15 09:39:16 -0500

    Updated version to 1.0.20 in build.gradle.kts. Modified button visibility in activity_formulario_actividad.xml to enhance user experience by hiding the button initially.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.uvrp.itsmantenimientoapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.0.19"
+        versionCode = 20
+        versionName = "1.0.20"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
